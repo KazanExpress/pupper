9d70d71681da27d6903b6a8c50a84fde2e36e5e6
Alik Khilazhev <alikhil@mail.ru>
2019-05-16 19:03:38 +0300

    Fix urlparsermiddleware

--- src/middlewares/urlParserMiddleware.js
+++ src/middlewares/urlParserMiddleware.js
@@ -3,5 +3,8 @@
 module.exports = function(req, res, next) {
     console.log(req.url);
     req.query.url = req.url.slice(1);
+    if (req.query.url === "_health") {
+        return
+    }
     next()
 };
